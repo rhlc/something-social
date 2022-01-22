4b5ef2b963e3ce74f953214b0a909be58a91cf28
Rahul <rahulcy7@gmail.com>
2022-01-22 12:29:17 +0530

    setup routes

---
+++ pages/[username]/[slug].js
@@ -0,0 +1,7 @@
+export default function PostPage({}) {
+  return (
+    <main>
+      <h1>Sign Up</h1>
+    </main>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,9 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <Link href="#">link</Link>
+    </div>
+  );
 }
