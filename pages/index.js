abf79dc1e01bf21621ee860aea55b14ba22b61ed
Rahul <rahulcy7@gmail.com>
2022-01-22 12:34:05 +0530

    add loader component

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Link from "next/link";
+import Loader from "../components/Loader";
 
 export default function Home() {
   return (
     <div>
+      <Loader show />
       <Link href="#">link</Link>
     </div>
   );
